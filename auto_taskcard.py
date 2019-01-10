61bb8d72639185e4476a3bb8a8991ca9376a5b44
Jaysome <gorillatactician@gmail.com>
2019-01-09 21:39:06 -0500

    finalized phase 1

--- auto_taskcard.py
+++ auto_taskcard.py
@@ -6,7 +6,7 @@ import sys
 import cv2
 
 pyautogui.FAILSAFE = True
-pyautogui.PAUSE = 1
+pyautogui.PAUSE = 0.5
 
 while True:
     while True:
@@ -52,15 +52,15 @@ while True:
     station = station.upper()
 
     while True:
-        print('TYPE: ', end='')  # TODO Modify name type is lame
+        print('RESOLUTION: ', end='')
         time.sleep(0.1)
-        pyautogui.typewrite('INSP/CHK')
-        inspchk = input()
-        inspchk.strip()
-        if len(inspchk) > 0:
+        pyautogui.typewrite('INSP'+'/'+'CHK')
+        resolution = input()
+        resolution.strip()
+        if len(resolution) > 0:
             break
         print('INVALID.')
-    inspchk = inspchk.upper()
+    resolution = resolution.upper()
 
     print('WORK ACCOMPLISHED: ', end='')
     time.sleep(0.1)
@@ -77,7 +77,7 @@ while True:
             break
         print('INVALID. Logpage must be numbers only')
 
-    traxData = [name, date, hr, mn, station, inspchk, work, logpage]
+    traxData = [name, date, hr, mn, station, resolution, work, logpage]
 
 
     def printpicnic(items_dict, left_width, right_width):
@@ -87,7 +87,7 @@ while True:
 
 
     traxDict = {'MECHANIC': name, 'DATE': date, 'HR:MN': hr + ':' + mn,
-                'TYPE': inspchk, 'STATION': station, 'LOGPAGE': logpage}
+                'RESOLUTION': resolution, 'STATION': station, 'LOGPAGE': logpage}
 
     printpicnic(traxDict, 10, 10)
 
@@ -104,7 +104,7 @@ while True:
 # date = '01/05/2019'
 # hr = '12'
 # mn = '00'
-# inspchk = 'INSP/CHK'
+# resolution = 'INSP/CHK'
 # station = 'YUL'
 # work = 'WORK ACCOMPLISHED AS PER TASK CARD INSTRUCTIONS.'
 # logpage = '12345'
@@ -113,14 +113,21 @@ while True:
 restartCondition = True
 
 
-def clickntype(clicklocation, text):
+def clickntype(clicklocation, text, backspaces):
     pyautogui.doubleClick(clicklocation)
+    pyautogui.typewrite('\b' * backspaces)
     pyautogui.typewrite(text, interval=0.1)
 
 
-def tabntype(numberoftabs, text):
+def tabntype(numberoftabs, text, backspaces):
     pyautogui.typewrite('\t' * numberoftabs)
+    pyautogui.typewrite('\b' * backspaces)
     pyautogui.typewrite(text, interval=0.1)
+
+
+def statusimplem(coordinatesofstatus):
+    pyautogui.click(coordinatesofstatus)
+    pyautogui.typewrite('c')
 
 
 def restarter():
@@ -156,31 +163,31 @@ try:
                     y = coords[1]
                     break
                 print('Cannot find an opened trax task card')
-
+            # TODO fix status bug +80 a lautre bout du monde +100 a coter
             statusLoc = (x + 160, y + 80)  # *OLD CORDS* X+150 Y+160
             byLoc = (x + 160, y + 127)  # x + 150, y + 205
             dateLoc = (x + 480, y + 193)  # x + 500, y + 270
             hrLoc = (x + 530, y + 193)  # x + 550, y + 270
             mnLoc = (x + 550, y + 193)  # x + 570, y + 270
             stationLoc = (x + 610, y + 193)  # x + 600, y + 270
-            inspchkLoc = (x + 610, y + 170)  # tentative location
+            resolutionLoc = (x + 610, y + 158)
             workLoc = (x + 300, y + 300)  # x + 300, y + 380
             saveIconLoc = (x, y)  # x + 17, y + 80
             workTabLoc = (x + 480, y + 520)  # x + 500, y + 600
             logPageLoc = (x + 540, y + 30)  # x + 560, y + 105
 
-
-            # TODO implement status behavior
-            clickntype(byLoc, name)
-            clickntype(dateLoc, date)
-            clickntype(hrLoc, hr)
-            clickntype(mnLoc, mn)
-            clickntype(stationLoc, station)
-            clickntype(inspchkLoc, inspchk)
-            clickntype(workLoc, work)
+            pyautogui.doubleClick(statusLoc)
+            pyautogui.typewrite('c')
+            clickntype(byLoc, name, 10)
+            clickntype(dateLoc, date, 0)
+            clickntype(hrLoc, hr, 0)
+            clickntype(mnLoc, mn, 0)
+            clickntype(stationLoc, station, 5)
+            clickntype(resolutionLoc, resolution, 0)
+            clickntype(workLoc, work, 0)
             pyautogui.doubleClick(workTabLoc)
             time.sleep(2)
-            clickntype(logPageLoc, logpage)
+            clickntype(logPageLoc, logpage, 0)
 
             print('AUTO TRAX COMPLETE')
             restarter()
@@ -189,35 +196,35 @@ try:
         # ----------------Tab Mode---------------- #
         while restartCondition:
             # tentative tab values
-            statusToBy = 2
-            byToInsp = 2
-            inspToDate = 1
+            statusToBy = 4
+            byToRes = 1
+            resToDate = 1
             dateToHr = 1
-            hrToMn = 1
+            hrToMn = 0
             mnToStation = 1
             stationToWork = 1
-            workToLogpage = 6
+            workToLogpage = 3
 
             while True:
                 coords = pyautogui.locateCenterOnScreen('status.png')
                 if coords is not None:
                     break
                 print('Cannot find an opened trax task card')
-            # potential implementation of status
-            # pyautogui.click(coords)
-            # pyautogui.typewrite(['c','enter'], interval = 0.2)
 
-            tabntype(statusToBy, name)
-            tabntype(byToInsp, inspchk)
-            tabntype(inspToDate, date)
-            tabntype(dateToHr, hr)
-            tabntype(hrToMn, mn)
-            tabntype(mnToStation, station)
-            tabntype(stationToWork, work)
+            #statusimplem(coords)
+            pyautogui.doubleClick(coords)
+            pyautogui.typewrite('c')
+            tabntype(statusToBy, name, 10)
+            tabntype(byToRes, resolution, 0)
+            tabntype(resToDate, date, 0)
+            tabntype(dateToHr, hr, 0)
+            tabntype(hrToMn, mn, 0)
+            tabntype(mnToStation, station, 5)
+            tabntype(stationToWork, work, 0)
 
-            workCoords = pyautogui.locateCenterOnScreen('workAccomplished.png', confidence=0.8)
+            workCoords = pyautogui.locateCenterOnScreen('workAccomplished.png', confidence=0.9)
             pyautogui.click(workCoords)
-            tabntype(workToLogpage, logpage)
+            tabntype(workToLogpage, logpage, 0)
 
             print('AUTO TRAX COMPLETE')
             restarter()
